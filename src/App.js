b2f74aad5290dcabba82f05aff1373d6cac9a1f8
[Github <[Github>
2023-10-19 20:56:01 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import "./App.css";
+import { useEffect, useState } from 'react';
+import './App.css';
+import socket from './server';
 
 function App() {
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    askUserName();
+  }, []);
+  const askUserName = () => {
+    const userName = prompt('당신의 이름을 입력하세요');
+    // console.log('uuu', userName);
+
+    socket.emit('login', userName, (res) => {
+      if (res?.ok) {
+        setUser(res.data);
+      }
+      console.log('Res', res);
+    });
+  };
   return (
     <div>
-      <div className="App"></div>
+      <div className='App'></div>
     </div>
   );
 }
