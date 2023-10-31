3afc20eba73736ca402488dffa3c21791ce193c4
[Github <[Github>
2023-10-31 18:54:28 +0900

    1031 commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import socket from './server';
+import InputField from './components/InputField/InputField';
+import MessageContainer from './components/MessageContainer/MessageContainer';
 
 function App() {
   const [user, setUser] = useState(null);
+  const [message, setMessage] = useState('');
+  const [messageList, setMessageList] = useState([]);
+  console.log('messageList', messageList);
   useEffect(() => {
     askUserName();
+    socket.on('message', (message) => {
+      setMessageList((prevState) => prevState.concat(message));
+    });
   }, []);
+
   const askUserName = () => {
     const userName = prompt('당신의 이름을 입력하세요');
     // console.log('uuu', userName);
@@ -18,9 +27,23 @@ function App() {
       console.log('Res', res);
     });
   };
+  const sendMessage = (event) => {
+    event.preventDefault();
+    socket.emit('sendMessage', message, (res) => {
+      console.log('sendMessage res', res);
+    });
+  };
+
   return (
     <div>
-      <div className='App'></div>
+      <div className='App'>
+        <MessageContainer messageList={messageList} user={user} />
+        <InputField
+          message={message}
+          setMessage={setMessage}
+          sendMessage={sendMessage}
+        />
+      </div>
     </div>
   );
 }
--- src/components/InputField/InputField.jsx
+++ src/components/InputField/InputField.jsx
@@ -1,31 +1,33 @@
-import React from 'react'
-import { Input } from "@mui/base/Input";
-import { Button } from "@mui/base/Button";
-import './InputField.css'
-const InputField = ({message,setMessage,sendMessage}) => {
+import React from 'react';
+import { Input } from '@mui/base/Input';
+import { Button } from '@mui/base/Button';
+import './InputField.css';
+const InputField = ({ message, setMessage, sendMessage }) => {
+  const handleSendMessage = (event) => {
+    event.preventDefault();
+    // 메시지를 보낸 후 메시지 상태 초기화
+    sendMessage(event);
+    setMessage(''); // 메시지를 초기화하여 입력창을 비웁니다.
+  };
 
   return (
-    <div className="input-area">
-          <div className="plus-button">+</div>
-          <form onSubmit={sendMessage} className="input-container">
-            <Input
-              placeholder="Type in here…"
-              value={message}
-              onChange={(event) => setMessage(event.target.value)}
-              multiline={false}
-              rows={1}
-            />
+    <div className='input-area'>
+      <div className='plus-button'>+</div>
+      <form onSubmit={handleSendMessage} className='input-container'>
+        <Input
+          placeholder='Type in here…'
+          value={message}
+          onChange={(event) => setMessage(event.target.value)}
+          multiline={false}
+          rows={1}
+        />
 
-            <Button
-              disabled={message === ""}
-              type="submit"
-              className="send-button"
-            >
-              전송
-            </Button>
-          </form>
-        </div>
-  )
-}
+        <Button disabled={message === ''} type='submit' className='send-button'>
+          전송
+        </Button>
+      </form>
+    </div>
+  );
+};
 
-export default InputField
+export default InputField;
